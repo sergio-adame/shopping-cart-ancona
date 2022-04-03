096a1c50bbdb72d09567c07420a7b59b87ac3338
Sergio Adame <sergioadameva@gmail.com>
2022-04-03 04:58:24 -0500

    Updates

---
+++ precache-manifest.38a92b85a4d88bc958fc4c8e086821d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d2b05f70c0d533bb9be77eb0862856",
+    "url": "/shopping-cart-ancona/index.html"
+  },
+  {
+    "revision": "97ff14013076159a7c5a",
+    "url": "/shopping-cart-ancona/static/css/main.8ae46512.chunk.css"
+  },
+  {
+    "revision": "25fa5106a9a928f8e3e4",
+    "url": "/shopping-cart-ancona/static/js/2.8f4d6a19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart-ancona/static/js/2.8f4d6a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ff14013076159a7c5a",
+    "url": "/shopping-cart-ancona/static/js/main.af94b9a9.chunk.js"
+  },
+  {
+    "revision": "590badffdb54c43e37ed",
+    "url": "/shopping-cart-ancona/static/js/runtime-main.fc25615f.js"
+  },
+  {
+    "revision": "3a60e3be16c3d7ff5e23b7501cf13afc",
+    "url": "/shopping-cart-ancona/static/media/AnconaAutopartes.3a60e3be.png"
+  }
+]);
